f3fa45786980553f759c60377367bf441f09646a
Luccas Calderaro <luccashmc@hotmail.com>
2019-12-12 20:31:59 -0300

    Testando classe de dominio Categoria

--- src/main/java/com/luccascalderaro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luccascalderaro/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,31 @@
 package com.luccascalderaro.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.luccascalderaro.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value ="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "REST esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2= new Categoria(2, "Escritorio");
+		
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+	
+		
+		
+		return list;
 	}
 
 }
