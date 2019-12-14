26bb950fe78d89e8263814546aa025e748ae1f25
Luccas Calderaro <luccashmc@hotmail.com>
2019-12-14 13:41:32 -0300

    Criando operacao de instanciacao

--- src/main/java/com/luccascalderaro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luccascalderaro/cursomc/resources/CategoriaResource.java
@@ -1,8 +1,5 @@
 package com.luccascalderaro.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -28,5 +25,6 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 		
 	}
-
+	
+	
 }
