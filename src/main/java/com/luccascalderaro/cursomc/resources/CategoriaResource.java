a927eefa7f3d681b182d2c5d70b616a9340f5a87
Luccas Calderaro <luccashmc@hotmail.com>
2019-12-29 13:49:39 -0300

    Deletando uma categoria com DELETE

--- src/main/java/com/luccascalderaro/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/luccascalderaro/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -49,5 +50,12 @@ public class CategoriaResource {
 		
 	}
 	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		
+		return ResponseEntity.noContent().build();
+		
+	}
 	
 }
--- src/main/java/com/luccascalderaro/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/luccascalderaro/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.luccascalderaro.cursomc.services.exceptions.DataIntegrityException;
 import com.luccascalderaro.cursomc.services.exceptions.ObjectNotFoundException;
 import com.luccascalderaro.cursomc.services.exceptions.StandardError;
 
@@ -20,4 +21,12 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
 	}
 
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(ObjectNotFoundException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
 }
