db3e6cb47e58ceb45f8a4452e1b55bbcd72f43f5
Luccas Calderaro <luccashmc@hotmail.com>
2020-01-30 20:08:58 -0300

    Enviando imagem via endpoint

--- src/main/java/com/luccascalderaro/cursomc/resources/ClienteResource.java
+++ src/main/java/com/luccascalderaro/cursomc/resources/ClienteResource.java
@@ -20,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luccascalderaro.cursomc.domain.Cliente;
@@ -94,6 +95,14 @@ public class ClienteResource {
 				path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
 		
+		
+	}
+	
+	@Transactional
+	@PostMapping(value="/picture")
+	public ResponseEntity<Void> uploadProfilePicture(@RequestParam(name="file") MultipartFile file){
+		URI uri = service.uploadProfilePicture(file);
+		return ResponseEntity.created(uri).build();
 		
 	}
 	
